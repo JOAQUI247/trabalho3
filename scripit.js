4bdf797d6dc900893bd97200605cde21fd4064a3
Joaquim D´avila da Silva <joaquimdavilasilva01@gmail.com>
2024-11-19 15:23:25 -0300

    Add files via upload

---
+++ scripit.js
@@ -0,0 +1,11 @@
+const pessoa = {};
+
+function mostrar() {
+    let resultado = document.getElementById
+    ('resultado')
+    console.log( resultado );
+
+    resultado.style.display = "block"
+
+    let nome = document.getElementById('nome').value;
+}
